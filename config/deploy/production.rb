7a52f389b1519597e902ddd4444b69e1fe870438
David Chmura <dschmura@humbledaisy.com>
2018-02-28 15:17:52 -0500

    UPDATE: Capistrano Deploy Scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,27 +1,38 @@
 # Change these
-server '35.227.87.83', port: 22, roles: [:web, :app, :db], primary: true
+# server '35.227.87.83', port: 22, roles: [:web, :app, :db], primary: true
+#
+# set :repo_url,        'git@github.com:mickn/seeker.git'
+# set :application,     'seeker'
+# set :user,            'mick'
+# set :puma_threads,    [4, 16]
+# set :puma_workers,    0
+#
+# # Don't change these unless you know what you're doing
+# set :pty,             true
+# set :use_sudo,        false
+# set :stage,           :production
+# set :deploy_via,      :remote_cache
+# set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
-set :repo_url,        'git@github.com:mickn/seeker.git'
-set :application,     'seeker'
-set :user,            'mick'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+server "159.203.3.130", user: 'deployer', roles: %w{app db web}
+set :stage, :production
+set :branch, :master
+set :deploy_user, "deployer"
+set :deploy_to, "/home/#{fetch(:deploy_user)}/apps/pedagogically_website"
+set :branch, 'master'
+set :rails_env, 'production'
+set :puma_env, "production"
+set :puma_conf, "#{shared_path}/config/puma.rb"
 
-# Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
@@ -35,6 +46,9 @@ set :puma_init_active_record, false  # Change to true if using ActiveRecord
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 set :linked_files, fetch(:linked_files, []).push('.env')
+set :linked_files, %w{config/puma.rb config/master.key config/database.yml config/global_config.yml}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# set :linked_dirs, fetch(:linked_dirs, []).push('public/packs', 'node_modules')
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
@@ -68,6 +82,16 @@ namespace :deploy do
     end
   end
 
+  desc 'Upload to shared/config'
+  task :upload do
+  on roles (:app) do
+    upload! "config/master.key",  "#{shared_path}/config/master.key"
+    upload! "config/puma.sample.rb",  "#{shared_path}/config/puma.rb"
+    upload! "config/database.yml", "#{shared_path}/config/database.yml"
+    upload! "config/global_config.yml", "#{shared_path}/config/global_config.yml"
+    end
+  end
+
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -84,4 +108,3 @@ end
 # ps aux | grep puma    # Get puma pid
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
-
